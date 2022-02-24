6e34fcc0ac98bd0d667dc5dfc2b0af59208f1bbb
husnuljahneer <husnuljahneer19@gmail.com>
2022-02-24 18:45:21 +0530

    first commit

---
+++ src/Services/pepperchatbot.js
@@ -0,0 +1,25 @@
+class Chat {
+
+    constructor(options = { name: "ChatBot" }) {
+        this.name = options.name ? options.name : "pepper bot",
+            this.owner = options.owner ? options.owner : "Husnul Jahneer"
+    }
+
+    async chat(message) {
+        if (!message) {
+            throw new Error("[Chatbot] Please provide some message to reply too!")
+        }
+
+        let json = await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message)}&botname=${encodeURIComponent(this.name)}&ownername=${encodeURIComponent(this.owner)}`)
+        let chat = await json.json();
+
+        if (!chat) {
+            throw new Error("[Chatbot] API Unavailable. Try again later.")
+        }
+
+        return chat;
+    }
+
+}
+
+module.exports = Chat
